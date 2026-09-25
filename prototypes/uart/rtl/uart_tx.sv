231c86778cc0ccea3a1f70fa7b122a7f79265007
Enoch Wong <enoch3d14@gmail.com>
2026-09-25 13:56:02 -0400

    UART TX continued work

--- prototypes/uart/rtl/uart_tx.sv
+++ prototypes/uart/rtl/uart_tx.sv
@@ -1,10 +1,14 @@
 `timescale 1ns / 1ps
 
-module uart_tx(
+module uart_tx #parameter (
+  BAUD_RATE = 115200,
+  DATA_WIDTH = 8
+  )
+(
   input logic clk,
   input logic rst_n,
   input logic start,
-  input logic [7:0] data_in,
+  input logic [DATA_WIDTH - 1:0] data_in,
   output logic tx,
   output logic busy,
   output logic done
@@ -18,6 +22,27 @@ module uart_tx(
     STOP
     } state_t;
 
+  // state registers
+  logic [1:0] next_state;
+
+  always_ff @(posedge clk or negedge rst_n) begin
+    if (!rst_n) begin
+      state <= IDLE;
+    end else begin
+      state <= next_state;
+    end
+  end
+  
+  // baud rate logic 
   
 
+  // next state logic
+  always_comb begin
+    unique case (state)
+      IDLE : next_state = start ? START : IDLE;
+      START : next_state = DATA;
+      DATA : next_state = data_last ? STOP : DATA;
+      STOP : next_state = IDLE;
+    endcase
+  end
 endmodule;
